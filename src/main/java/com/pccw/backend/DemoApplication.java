eefa010b47d4722ff000de263f22ccce4c551a0c
ivanc0528 <67098484+ivanc0528@users.noreply.github.com>
2020-09-28 16:59:19 +0800

    Update DemoApplication.java
    a test

--- src/main/java/com/pccw/backend/DemoApplication.java
+++ src/main/java/com/pccw/backend/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableSwagger2
